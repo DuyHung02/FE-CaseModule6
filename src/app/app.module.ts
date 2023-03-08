6f61b54ee530bae3873af90281b3dbb212a466ec
DuyHung02 <duyhung23102002@gmail.com>
2023-03-08 16:43:11 +0700

    done register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { RegisterComponent } from './account/register/register.component';
 import {ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 import { HomeIndexComponent } from './home/home-index/home-index/home-index.component';
-import { LoginComponent } from './account/login/login/login.component';
+import { LoginComponent } from './account/login/login.component';
 
 @NgModule({
   declarations: [
