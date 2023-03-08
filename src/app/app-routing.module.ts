fd666f0eb70cf363426f0dc8bced41922d22b599
DuyHung02 <duyhung23102002@gmail.com>
2023-03-08 16:41:51 +0700

    done register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {RegisterComponent} from "./account/register/register.component";
+import {HomeIndexComponent} from "./home/home-index/home-index/home-index.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'register', component: RegisterComponent},
+  {path: '', component: HomeIndexComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/models/AccountToken.ts
@@ -0,0 +1,14 @@
+
+export class AccountToken {
+  id? : number
+  username?: string
+  password?: string
+  full_name?: string
+  gender?: string
+  gmail?: String
+  address?: string
+  phone_number?: string
+  avatar?: string
+  roles? : any
+  token?: string
+}
