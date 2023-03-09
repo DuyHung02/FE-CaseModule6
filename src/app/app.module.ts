43d96b829ae59a6cc77be2c112613fa34e70d578
CodeGym <mr.hoanh19111998@gmail.com>
2023-03-09 08:09:09 +0700

    hoanh-up-trang-home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './home/header/header.component';
+import {FooterComponent} from "./home/footer/footer.component";
+import { BodyComponent } from './home/body/body.component';
+import { NewSongComponent } from './home/body/new-song/new-song.component';
+import { ChartsComponent } from './home/body/charts/charts.component';
+import { NavbarComponent } from './home/navbar/navbar.component';
+import { SingerComponent } from './home/body/singer/singer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent,
+    NewSongComponent,
+    ChartsComponent,
+    NavbarComponent,
+    SingerComponent
+
   ],
   imports: [
     BrowserModule,
